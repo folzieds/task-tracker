b0a928c4768f53d5980a572d4b9542c3d35374b1
folzieds <folzieds@gmail.com>
2023-01-04 00:07:04 +0100

    Added Button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import PropTypes from "prop-types";
+import Button from "./Button";
 const Header = ({ title }) => {
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
+            <Button color = 'black' text= 'Add'/>
         </header>
     )
 }
 
 Header.defaultProps = {
-    title: "Task tracker",
+    title: "Task Tracker",
 }
 
 Header.protoTypes = {

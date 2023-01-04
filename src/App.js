be513b22c15b57a846788d274043396201b496bb
folzieds <folzieds@gmail.com>
2023-01-04 01:23:59 +0100

    Added Tasks components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import {useState} from "react";
 
 function App() {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Buy Soap',
+            day: 'Jan 3, 2023 at 1:30pm',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: 'Clean Car',
+            day: 'Jan 3, 2023 at 2:30pm',
+            reminder: false,
+        },
+        {
+            id: 3,
+            text: 'Take out trash',
+            day: 'Jan 3, 2023 at 3:30 pm',
+            reminder: false,
+        },
+    ]);
+
   return (
     <div className="container">
         <Header />
+        <Tasks tasks={tasks} />
     </div>
   );
 }

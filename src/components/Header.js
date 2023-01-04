bf54827ce6f581a5678c20e3b353b3eb4b9edf49
folzieds <folzieds@gmail.com>
2023-01-04 01:03:38 +0100

    Added onclick event for button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('Clicked');
+    }
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color = 'black' text= 'Add'/>
+            <Button onClick={onClick} color = 'black' text= 'Add'/>
         </header>
     )
 }

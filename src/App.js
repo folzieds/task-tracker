599f48f5e6efb21cc6b4ba00c04e805ddf8fde1d
folzieds <folzieds@gmail.com>
2023-01-04 02:01:53 +0100

    Added Task component

--- src/App.js
+++ src/App.js
@@ -25,10 +25,15 @@ function App() {
         },
     ]);
 
+    // Delete a task
+    const deleteTask = (id) => {
+        setTasks(tasks.filter((task) => task.id !== id));
+    };
+
   return (
     <div className="container">
         <Header />
-        <Tasks tasks={tasks} />
+        {tasks.length > 0 ? <Tasks tasks={tasks}  onDelete= {deleteTask}/> : 'No Task to show'}
     </div>
   );
 }
